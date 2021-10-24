52ac5a35836cf1ab9eb3d2c33b413a672d3013f3
Alessandro Tatoli <v.tatoli@sitca-ve.com>
2021-10-24 15:00:26 -0400

    arreglo web.php de mike v2

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use http\Env\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class FormController extends Controller
